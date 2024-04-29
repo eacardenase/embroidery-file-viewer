c89f7f1894f962a5fcd21497c656b2ffffc713d8
Edwin Cardenas <eacardenase@gmail.com>
2024-04-29 12:56:18 -0500

    feat: main.py cleanup

--- main.py
+++ main.py
@@ -14,74 +14,11 @@ import os
 
 app = FastAPI()
 
-class Extras(BaseModel):
-    name: str
-    ST: str
-    CO: str
-    X_positive: str = Field(..., alias='+X')
-    X_negative: str = Field(..., alias='-X')
-    Y_positive: str = Field(..., alias='+Y')
-    Y_negative: str = Field(..., alias='-Y')
-    AX: str
-    AY: str
-    MX: str
-    MY: str
-    PD: str
-
-
-class EmbroideryFileData(BaseModel):
-    threadlist: List
-    stitches: List[List[Union[int, str]]]
-    extras: Extras
-
-class EmbroideryData(BaseModel):
-    name: str
-    colors: list[str]
-    embroidery_file_data: EmbroideryFileData 
-
-@app.get("/embroidery-items/")
-async def read_embroidery_data():
-    try:
-        with open("./embroidery_files/embroidery_data.json", "r") as file:
-            pattern = pyembroidery.read_json(file)
-            directory_path = "./embroidery_images/"
-
-            if not os.path.exists(directory_path):
-                os.makedirs(directory_path)
-
-            file_path = os.path.join(directory_path, "embroidery_data.png")
-
-            pattern.write_png(pattern, file_path)
-
-        return FileResponse(file_path)
-    except FileNotFoundError:
-        raise HTTPException(status_code=404, detail="File not found")
-    except json.JSONDecodeError:
-        raise HTTPException(status_code=422, detail="Error decoding JSON")
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-@app.post("/embroidery-items/")
-async def create_file(
-    embroidery_data: EmbroideryData,
-):
-    try:
-        item_data = embroidery_data.embroidery_file_data.dict()
-        directory_path = "./embroidery_files/"
-        
-        if not os.path.exists(directory_path):
-            os.makedirs(directory_path)
-        
-        file_path = os.path.join(directory_path, "embroidery_data.json")
-        
-        with open(file_path, 'w') as f:
-            json.dump(item_data, f, indent=4)
-
-        return {"message": "Embroidery data saved successfully!", "filename": file_path}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+@app.get('/')
+def home():
+    return {"status": 200}
     
-@app.post("/embroidery-items-2")
+@app.post("/embroidery-items")
 async def create_file(
     file: UploadFile,
     colors: Annotated[str, Form()]
@@ -91,9 +28,7 @@ async def create_file(
         images_directory_path = "./embroidery_images/"
 
         hex_colors = ast.literal_eval(colors)
-
         file_name = file.filename
-
         file_bytes = await file.read()
 
         if not os.path.exists(dst_directory_path):
@@ -105,7 +40,6 @@ async def create_file(
             f.write(file_bytes)
 
         pattern = pyembroidery.read_dst(dst_file_path)
-
         png_file_path = os.path.join(images_directory_path, file_name.replace('.DST', '.png'))
 
         for hex_color in hex_colors:
